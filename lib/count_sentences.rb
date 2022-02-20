e377f3ec1492e6b7089ea3dd7ed96e8e787f77e8
ajlic24@gmail.com <ajlic24@gmail.com>
2022-02-20 08:05:51 -0500

    commit tree 94a9af05bca81438ca07ccb4596f72e76d34fd71

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self[-1] === '.'
   end
 
   def question?
-
+    self[-1] === '?'
   end
 
   def exclamation?
-
+    self[-1] === '!'
   end
 
   def count_sentences
-
+    self.scan(/\w+[.?!]+/).length
   end
 end
+
+# puts "This is a string! It has three sentences. Right??".count_sentences
